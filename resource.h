cf7133d39358dee281f255ccde8704179811caf1
kuna <kgdgwn98@gmail.com>
2012-07-09 14:56:41 +0900

    12/7/9
    compilable commit

---
+++ resource.h
@@ -0,0 +1,32 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by LR2Twit.rc
+//
+#define IDC_MYICON                      2
+#define IDD_LR2TWIT_DIALOG              102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_LR2TWIT                     107
+#define IDI_SMALL                       108
+#define IDC_LR2TWIT                     109
+#define IDR_MAINFRAME                   128
+#define IDI_LR2_E                       129
+#define IDI_LR2_D                       130
+#define IDR_NOTIFY                      132
+#define ID_MENU_SETTINGS                32771
+#define ID_MENU_EXIT                    32772
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        133
+#define _APS_NEXT_COMMAND_VALUE         32773
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
---
+++ stdafx.cpp
@@ -0,0 +1,16 @@
+// stdafx.cpp : source file that includes just the standard includes
+// LR2Twit.pch will be the pre-compiled header
+// stdafx.obj will contain the pre-compiled type information
+
+#include "stdafx.h"
+
+// TODO: reference any additional headers you need in STDAFX.H
+// and not in this file
+
+ int opt1; // auto twit
+ int opt2; // clear
+ int opt3; // high score
+ int opt4; // 't' press key
+ TCHAR opt_lr2title[255];
+ char opt_encode[255];
+ TCHAR opt_message[255];
